9fb7d654f3cc4989fb6b3dee4de9c98c7c04ac3e
HuzaifaQayyum <huzaifaacf@gmail.com>
2022-02-22 23:59:00 +0500

    DEBUG set to false or ENV var

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,8 +2,7 @@ from .base import *
 import os
 import dj_database_url
 
-# DEBUG = os.environ.get('DEBUG', False)
-DEBUG=True
+DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(', ')
 
 SECRET_KEY = os.environ['SECRET_KEY']
